c17f6b0fb35d2b74fb2b1d856c30a9fa3120b5ec
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-08-02 13:56:48 -0500

    COMMIT INICIAL, PROYECTO EN BLANCO

---
+++ src/main/java/com/Movie_Spring/MovieSteamApi_Backend_Spring/MovieSteamApiBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.Movie_Spring.MovieSteamApi_Backend_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSteamApiBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSteamApiBackendSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Movie_Spring/MovieSteamApi_Backend_Spring/MovieSteamApiBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Movie_Spring.MovieSteamApi_Backend_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieSteamApiBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
