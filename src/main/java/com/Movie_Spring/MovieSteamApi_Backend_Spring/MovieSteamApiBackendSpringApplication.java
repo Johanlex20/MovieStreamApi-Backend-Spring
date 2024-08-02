92e29191492706fd64c9d4623323d1c4192d8bbd
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-08-02 17:41:12 -0500

    Refactorizando para trabajar con REST, liberamos la clase principal, Crear ApiServie quien va tener la logica de consumo api Externa, Add anotaciones Consumo api @Configuration y @Services

--- src/main/java/com/Movie_Spring/MovieSteamApi_Backend_Spring/MovieSteamApiBackendSpringApplication.java
+++ src/main/java/com/Movie_Spring/MovieSteamApi_Backend_Spring/MovieSteamApiBackendSpringApplication.java
@@ -1,46 +1,13 @@
 package com.Movie_Spring.MovieSteamApi_Backend_Spring;
-import com.Movie_Spring.MovieSteamApi_Backend_Spring.Services.ConvertirDatos;
-import com.Movie_Spring.MovieSteamApi_Backend_Spring.configuration.ConsumoApiMovieDB;
-import com.Movie_Spring.MovieSteamApi_Backend_Spring.models.DatoApi;
-import com.Movie_Spring.MovieSteamApi_Backend_Spring.models.SerieDto;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieSteamApiBackendSpringApplication implements CommandLineRunner {
+public class MovieSteamApiBackendSpringApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieSteamApiBackendSpringApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("PROBANDO API");
-
-		ConsumoApiMovieDB consumoApiMovieDB = new ConsumoApiMovieDB();
-		ConvertirDatos convertirDatos = new ConvertirDatos();
-
-		//OBTNER LOS DATOS TAL CUAL LLEGAN DEL API EXTERNA
-		var json = consumoApiMovieDB.obtenerDatosApi("https://api.themoviedb.org/3/search/tv?query=Friends&include_adult=false&language=es-ES");
-		System.out.println(json);
-
-		//CONVERTIR LOS DATOS EXTERNOS A DATOS JAVA
-		var datos = convertirDatos.convertirDatos(json, DatoApi.class);
-		//System.out.println(datos);
-
-		//OBTENER SOLO EL PRIMER RESLUTADO DEL GRUPO DE DATOS VERIFICAMOS QUE NO SEA NULL Y NO ESTE VACIO
-		//ESTA PARTE SE DEBE HACER PARA OBTENER EL ID DE LA SERIE YA QUE CON ESTE VAMOS A OBTENER MAS DATOS CON OTRO URL
-		if (datos.getResultado() != null && !datos.getResultado().isEmpty()){
-			SerieDto primerDato = datos.getResultado().get(0);
-			System.out.println("id Serie: "+ primerDato.idSerie() +" titulo:" + primerDato.titulo());
-		}
-
-	}
-
-
-
-
 }
 
 
---
+++ src/main/java/com/Movie_Spring/MovieSteamApi_Backend_Spring/Services/ApiService.java
@@ -0,0 +1,31 @@
+package com.Movie_Spring.MovieSteamApi_Backend_Spring.Services;
+import com.Movie_Spring.MovieSteamApi_Backend_Spring.configuration.ConsumoApiMovieDB;
+import com.Movie_Spring.MovieSteamApi_Backend_Spring.models.DatoApi;
+import com.Movie_Spring.MovieSteamApi_Backend_Spring.models.SerieDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiService {
+
+    private final String  API_BASE = "https://api.themoviedb.org/3/";
+    private final String API_IDIOMA_ES = "language=es-ES";
+    ConsumoApiMovieDB consumoApiMovieDB = new ConsumoApiMovieDB();
+    ConvertirDatos convertirDatos = new ConvertirDatos();
+
+    private SerieDto obtenerDatosSerie(String nombreSerie){
+        //OBTNER LOS DATOS TAL CUAL LLEGAN DEL API EXTERNA
+        var json = consumoApiMovieDB.obtenerDatosApi(API_BASE +"search/tv?query="+ nombreSerie.replace(" ", "%20") +"&include_adult=false&"+ API_IDIOMA_ES);
+
+        //CONVERTIR LOS DATOS EXTERNOS A DATOS JAVA
+        var datos = convertirDatos.convertirDatos(json, DatoApi.class);
+
+
+        //OBTENER SOLO EL PRIMER RESLUTADO DEL GRUPO DE DATOS VERIFICAMOS QUE NO SEA NULL Y NO ESTE VACIO, ESTA PARTE SE DEBE HACER PARA OBTENER EL ID DE LA SERIE YA QUE CON ESTE VAMOS A OBTENER MAS DATOS CON OTRO URL
+        if (datos.getResultado() != null && !datos.getResultado().isEmpty()){
+            SerieDto primerDato = datos.getResultado().get(0);
+            json = consumoApiMovieDB.obtenerDatosApi(API_BASE +"/tv" + primerDato.idSerie() + "?" + API_IDIOMA_ES);
+            return convertirDatos.convertirDatos(json, SerieDto.class);
+        }
+        return null;
+    }
+}
